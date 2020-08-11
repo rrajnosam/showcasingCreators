ef0d4d96e72bc4077fb2241827011b632a3cabe3
Mason Jarr <rrajnosam@gmail.com>
2020-08-11 15:22:02 -0700

    deployment initial

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -6,12 +6,12 @@ const Tag = require("../models/tag-model.js")
 
 router.get("/", authCheck, paginate, async (req, res) => {
     try {
-        // if ((req.user._id == process.env.ADMIN_ID) || (req.user._id == process.env.ADMIN_ID1)) {
-        res.render("tag-submit.ejs", { user: req.user, paginate: res.paginate })
+        if ((req.user._id == process.env.ADMIN_ID) || (req.user._id == process.env.ADMIN_ID1)) {
+            res.render("tag-submit.ejs", { user: req.user, paginate: res.paginate })
 
-        // } else {
-        //     res.status(401).send("unauthorized")
-        // }
+        } else {
+            res.status(401).send("unauthorized")
+        }
     } catch (err) {
         console.log(err)
     }
@@ -20,15 +20,15 @@ router.get("/", authCheck, paginate, async (req, res) => {
 router.post("/", authCheck, (req, res) => {
     // console.log("tag post")
     try {
-        // if ((req.user._id == process.env.ADMIN_ID) || (req.user._id == process.env.ADMIN_ID1)) {
-        new Tag({
-            name: req.body.newTag
-        }).save()
-            .catch((err) => console.log(err))
-        res.redirect("/tag-submit")
-        // } else {
-        //     res.status(401).send("unauthorized")
-        // }
+        if ((req.user._id == process.env.ADMIN_ID) || (req.user._id == process.env.ADMIN_ID1)) {
+            new Tag({
+                name: req.body.newTag
+            }).save()
+                .catch((err) => console.log(err))
+            res.redirect("/tag-submit")
+        } else {
+            res.status(401).send("unauthorized")
+        }
     } catch (err) {
         console.log(err)
     }
