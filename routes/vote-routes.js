431dd582bd19f1487afd35b6dbd5dcf87dc31a8d
Mason Jarr <rrajnosam@gmail.com>
2020-09-08 17:06:47 -0700

    fix alignment bug in homepage. unlimited vote for admin

--- routes/vote-routes.js
+++ routes/vote-routes.js
@@ -13,95 +13,124 @@ router.post("/", authCheck, (req, res) => {
     let votes = "0"
     let disabled = { up: false, down: false }
 
-    User.findOne({ _id: req.user._id, "votedChannels.channel": response.id })
-        .then((foundUser) => {
-            if (foundUser) {
-                // console.log(foundUser)
-                const votedChannelsArray = foundUser.votedChannels.map((index) => String(index.channel))
-                const vidIndex = votedChannelsArray.indexOf(response.id)
-                const video = foundUser.votedChannels[vidIndex].channel
-                const pastDirection = foundUser.votedChannels[vidIndex].direction
+    if (req.user.id == "5f498004d8128d0004ebf15a" || req.user.id == "5f497d12d8128d0004ebf12d" || req.user.id == "5f4994c5c2d39b00049073eb" || req.user.id == "5f48326ad8128d0004ebf098" || req.user.id == "5f483265d8128d0004ebf097") {
+        if (response.direction === "up") {
+            Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: 1 } }, { new: true })
+                .then((foundChannel) => {
+                    // console.log(foundChannel)
+                    votes = String(foundChannel.votes)
+                    disabled = { up: false, down: false }
+                    res.json({
+                        votes: votes,
+                        disabled
+                    })
+                }).catch((err) => { console.error(err) })
 
-                // console.log("cancel the vote")
-                if ((response.direction === "up" || response.direction === "down") && pastDirection === "up") {
-                    Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: -1 } }, { new: true })
-                        .then((returned) => {
-                            User.updateOne({ _id: foundUser._id }, { $pull: { votedChannels: { channel: video } } })
-                                .then((updated) => {
-                                    votes = String(returned.votes)
-                                    disabled = { up: false, down: false }
-                                    res.json({
-                                        votes: votes,
-                                        disabled
-                                    })
-                                }).catch((err) => {
-                                    console.log(err)
-                                })
+        } else if (response.direction === "down") {
+            Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: -1 } }, { new: true })
+                .then((foundChannel) => {
+                    // console.log(foundChannel)
+                    votes = String(foundChannel.votes)
+                    disabled = { up: false, down: false }
+                    res.json({
+                        votes: votes,
+                        disabled
+                    })
+                }).catch((err) => { console.error(err) })
+        }
+    } else {
 
-                        }).catch((err) => {
-                            console.log(err)
-                        })
-                } else if ((response.direction === "down" || response.direction === "up") && pastDirection === "down") {
-                    Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: 1 } }, { new: true })
-                        .then((returned) => {
-                            User.findOneAndUpdate({ _id: foundUser._id }, { $pull: { votedChannels: { channel: video } } })
-                                .then((updated) => {
-                                    votes = String(returned.votes)
-                                    disabled = { up: false, down: false }
-                                    res.json({
-                                        votes: votes,
-                                        disabled
+
+        User.findOne({ _id: req.user._id, "votedChannels.channel": response.id })
+            .then((foundUser) => {
+                if (foundUser) {
+                    // console.log(foundUser)
+                    const votedChannelsArray = foundUser.votedChannels.map((index) => String(index.channel))
+                    const vidIndex = votedChannelsArray.indexOf(response.id)
+                    const video = foundUser.votedChannels[vidIndex].channel
+                    const pastDirection = foundUser.votedChannels[vidIndex].direction
+
+                    // console.log("cancel the vote")
+                    if ((response.direction === "up" || response.direction === "down") && pastDirection === "up") {
+                        Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: -1 } }, { new: true })
+                            .then((returned) => {
+                                User.updateOne({ _id: foundUser._id }, { $pull: { votedChannels: { channel: video } } })
+                                    .then((updated) => {
+                                        votes = String(returned.votes)
+                                        disabled = { up: false, down: false }
+                                        res.json({
+                                            votes: votes,
+                                            disabled
+                                        })
+                                    }).catch((err) => {
+                                        console.log(err)
                                     })
-                                }).catch((err) => {
-                                    console.log(err)
-                                })
+
+                            }).catch((err) => {
+                                console.log(err)
+                            })
+                    } else if ((response.direction === "down" || response.direction === "up") && pastDirection === "down") {
+                        Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: 1 } }, { new: true })
+                            .then((returned) => {
+                                User.findOneAndUpdate({ _id: foundUser._id }, { $pull: { votedChannels: { channel: video } } })
+                                    .then((updated) => {
+                                        votes = String(returned.votes)
+                                        disabled = { up: false, down: false }
+                                        res.json({
+                                            votes: votes,
+                                            disabled
+                                        })
+                                    }).catch((err) => {
+                                        console.log(err)
+                                    })
+
+                            }).catch((err) => {
+                                console.log(err)
+                            })
+                    }
+
+                } else {
+                    // console.log("this dude hasn't voted yet")
+                    User.updateOne({ _id: req.user._id }, { $addToSet: { votedChannels: { channel: response.id, direction: response.direction } } })
+                        .then((update) => {
+                            if (response.direction === "up") {
+                                Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: 1 } }, { new: true })
+                                    .then((foundChannel) => {
+                                        // console.log(foundChannel)
+                                        votes = String(foundChannel.votes)
+                                        disabled = { up: true, down: false }
+                                        res.json({
+                                            votes: votes,
+                                            disabled
+                                        })
+                                    }).catch((err) => { console.error(err) })
+
+                            } else if (response.direction === "down") {
+                                Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: -1 } }, { new: true })
+                                    .then((foundChannel) => {
+                                        // console.log(foundChannel)
+                                        votes = String(foundChannel.votes)
+                                        disabled = { up: false, down: true }
+                                        res.json({
+                                            votes: votes,
+                                            disabled
+                                        })
+                                    }).catch((err) => { console.error(err) })
+                            }
 
                         }).catch((err) => {
                             console.log(err)
                         })
                 }
 
-            } else {
-                // console.log("this dude hasn't voted yet")
-                User.updateOne({ _id: req.user._id }, { $addToSet: { votedChannels: { channel: response.id, direction: response.direction } } })
-                    .then((update) => {
-                        if (response.direction === "up") {
-                            Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: 1 } }, { new: true })
-                                .then((foundChannel) => {
-                                    // console.log(foundChannel)
-                                    votes = String(foundChannel.votes)
-                                    disabled = { up: true, down: false }
-                                    res.json({
-                                        votes: votes,
-                                        disabled
-                                    })
-                                }).catch((err) => { console.error(err) })
-
-                        } else if (response.direction === "down") {
-                            Channel.findOneAndUpdate({ _id: response.id }, { $inc: { votes: -1 } }, { new: true })
-                                .then((foundChannel) => {
-                                    // console.log(foundChannel)
-                                    votes = String(foundChannel.votes)
-                                    disabled = { up: false, down: true }
-                                    res.json({
-                                        votes: votes,
-                                        disabled
-                                    })
-                                }).catch((err) => { console.error(err) })
-                        }
-
-                    }).catch((err) => {
-                        console.log(err)
-                    })
-            }
 
 
+            })
+            .catch((err) => { console.error(err) })
 
-        })
-        .catch((err) => { console.error(err) })
+        // }
 
-    // }
-
+    }
 })
 
 
